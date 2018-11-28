3a30a79b3ff0e591105794d417068e6297f6ab47
Poerling <poerling.philipp@googlemail.com>
2018-11-29 00:09:33 +0100

    Update Kaempfer.java

--- src/hsMannheim/pr2/ws2018/grp3/pflichtuebung2/personen/natuerlichePersonen/Kaempfer.java
+++ src/hsMannheim/pr2/ws2018/grp3/pflichtuebung2/personen/natuerlichePersonen/Kaempfer.java
@@ -2,5 +2,5 @@ package hsMannheim.pr2.ws2018.grp3.pflichtuebung2.personen.natuerlichePersonen;
 
 public abstract interface Kaempfer {
 
-    public void kaempfe(Mutant gladiator);
+    public abstract void kaempfe(Mutant gladiator);
 }
