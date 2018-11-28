be5d031eb5000af979c667aa748e89d5acbf703d
Elvis <elvis.herbrandt@gmail.com>
2018-11-28 14:35:23 +0100

    grundlage für junit testing ist da, nur noch mit mehtoden füllen

---
+++ src/hsMannheim/pr2/ws2018/grp3/pflichtuebung2/JUnit/TestCase.java
@@ -0,0 +1,14 @@
+package hsMannheim.pr2.ws2018.grp3.pflichtuebung2.JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
